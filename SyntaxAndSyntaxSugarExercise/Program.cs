20d68b1c6737e9381c202d731a46cac68886d962
Cali Weber <calista.weber1@gmail.com>
2023-07-25 12:54:39 -0700

    finished exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+                                          
+           var answer = 4;
+           var response = ((answer < 9) ? "is less than nine" : (answer >= 9) ? "is great than 9" : "equal to nine");
+           Console.WriteLine(response);
+
+
+            
+
+            
+            
+            
+            
         }
+
     }
 }
